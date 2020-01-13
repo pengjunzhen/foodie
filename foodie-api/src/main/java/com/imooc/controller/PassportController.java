82fe7751492837ca9c8a6220257daf8b8387510f
pengjunzhen <pengjunzhenwork@163.com>
2020-01-13 21:30:37 +0800

    feat: 添加登陆、登出接口，以及若干工具类

--- foodie-api/src/main/java/com/imooc/controller/PassportController.java
+++ foodie-api/src/main/java/com/imooc/controller/PassportController.java
@@ -3,12 +3,18 @@ package com.imooc.controller;
 import com.imooc.pojo.Users;
 import com.imooc.pojo.vo.UserVO;
 import com.imooc.service.UserService;
+import com.imooc.utils.CookieUtils;
 import com.imooc.utils.JSONResult;
+import com.imooc.utils.JsonUtils;
+import com.imooc.utils.MD5Utils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Api(value = "注册登录", tags = {"用于注册登录的相关接口"})
 @RestController
@@ -38,7 +44,9 @@ public class PassportController {
 
     @ApiOperation(value = "用户注册", notes = "用户注册", httpMethod = "POST")
     @PostMapping("/regist")
-    public JSONResult regist(@RequestBody UserVO userVO) {
+    public JSONResult regist(@RequestBody UserVO userVO,
+                             HttpServletRequest request,
+                             HttpServletResponse response) {
         String username = userVO.getUsername();
         String password = userVO.getPassword();
         String confirmPwd = userVO.getConfirmPassword();
@@ -60,8 +68,65 @@ public class PassportController {
         }
 
         // 4、实现注册
-        Users user = userService.createUser(userVO);
+        Users userResult = userService.createUser(userVO);
+        userResult = setNullProperty(userResult);
+
+        CookieUtils.setCookie(request, response, "user",
+                JsonUtils.objectToJson(userResult), true);
 
         return JSONResult.ok();
+    }
+
+    @ApiOperation(value = "用户登陆", notes = "用户登陆", httpMethod = "POST")
+    @PostMapping("/login")
+    public JSONResult login(@RequestBody UserVO userVO,
+                            HttpServletRequest request,
+                            HttpServletResponse response) {
+        String username = userVO.getUsername();
+        String password = userVO.getPassword();
+
+        // 1、判断用户名和密码必须不为空
+        if (StringUtils.isBlank(username) || StringUtils.isBlank(password)) {
+            return JSONResult.errorMsg("用户名或密码不能为空");
+        }
+
+        // 2、密码长度不能少于6位
+        if (password.length() < 6) {
+            return JSONResult.errorMsg("密码长度不能小于6");
+        }
+
+        // 3、登陆
+        Users userResult = userService.queryUserForLogin(username, MD5Utils.getMD5Str(password));
+
+        if (userResult == null) {
+            return JSONResult.errorMsg("用户名或密码错误");
+        }
+
+        CookieUtils.setCookie(request, response, "user",
+                JsonUtils.objectToJson(userResult), true);
+
+        return JSONResult.ok(userResult);
+    }
+
+    @ApiOperation(value = "用户退出登录", notes = "用户退出登录", httpMethod = "POST")
+    @PostMapping("/logout")
+    public JSONResult logout(@RequestParam Long userId,
+                                  HttpServletRequest request,
+                                  HttpServletResponse response) {
+
+        // 清除用户的相关信息的cookie
+        CookieUtils.deleteCookie(request, response, "user");
+
+        return JSONResult.ok();
+    }
+
+    private Users setNullProperty(Users userResult) {
+        userResult.setPassword(null);
+        userResult.setMobile(null);
+        userResult.setEmail(null);
+        userResult.setCreatedTime(null);
+        userResult.setUpdatedTime(null);
+        userResult.setBirthday(null);
+        return userResult;
     }
 }
